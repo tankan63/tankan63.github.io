768bcb56485d681afa48018288a86e6cfb28f59b
tan <tanay@Mac-mini.local>
2023-08-22 00:01:29 -0500

    added new page

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// lastUpdated.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the last modified timestamp of the current page
+    var lastModified = new Date(document.lastModified);
+
+    // Extract the date components
+    var year = lastModified.getFullYear();
+    var month = lastModified.getMonth() + 1; // Months are 0-indexed
+    var day = lastModified.getDate();
+
+    // Update the HTML content of the 'lastUpdated' element
+    document.getElementById("lastUpdated").textContent = year + "-" + month + "-" + day;
+});
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var site_data = JSON.parse(this.responseText);
         var num_arr = site_data.info.views.toString().split("");
+        console.log(site_data);
         var num_str = "";
         for (i = 0; i < num_arr.length; i++) {
             num_str += num_arr[i];
