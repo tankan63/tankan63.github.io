a09e36dda23c4779e1f8675b5b9e3d6c81951e86
tan <tanay@Mac-mini.local>
2023-11-29 08:25:25 -0600

    linting

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 // lastUpdated.js
 document.addEventListener("DOMContentLoaded", function() {
     // Get the last modified timestamp of the current page
-    var lastModified = new Date(document.lastModified);
+    let lastModified = new Date(document.lastModified);
 
     // Extract the date components
-    var year = lastModified.getFullYear();
-    var month = lastModified.getMonth() + 1; // Months are 0-indexed
-    var day = lastModified.getDate();
+    let year = lastModified.getFullYear();
+    let month = lastModified.getMonth() + 1; // Months are 0-indexed
+    let day = lastModified.getDate();
 
     // Update the HTML content of the 'lastUpdated' element
     document.getElementById("lastUpdated").textContent = year + "-" + month + "-" + day;
 });
 
-var xhttp = new XMLHttpRequest();
+let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var site_data = JSON.parse(this.responseText);
-        var num_arr = site_data.info.views.toString().split("");
+    if (this.readyState === 4 && this.status === 200) {
+        let site_data = JSON.parse(this.responseText);
+        let num_arr = site_data.info.views.toString().split("");
         console.log(site_data);
-        var num_str = "";
+        let num_str = "";
         for (i = 0; i < num_arr.length; i++) {
             num_str += num_arr[i];
             if ( (num_arr.length-1 - i) % 3 == 0 && (num_arr.length-1 - i) != 0 ) {num_str += ",";}
-            var date_str = site_data.info.last_updated;
-            var date_obj = new Date(site_data.info.last_updated);
+            let date_str = site_data.info.last_updated;
+            let date_obj = new Date(site_data.info.last_updated);
             document.getElementById("lastupdate").innerHTML = (date_obj.getMonth()+1) + "-" + date_obj.getDate() + "-" + date_obj.getFullYear();
         }
         document.getElementById("hitcount").innerHTML = num_str;
